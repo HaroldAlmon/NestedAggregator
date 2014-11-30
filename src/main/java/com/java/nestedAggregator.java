4aa93186384285bfa42d7753c43490d5e10fb8c7
haroldalmon <haroldalmon@hotmail.com>
2014-11-29 20:39:22 -0600

    remove comments

--- src/main/java/com/java/nestedAggregator.java
+++ src/main/java/com/java/nestedAggregator.java
@@ -20,16 +20,9 @@ import org.springframework.scheduling.annotation.Async;
 
 public class nestedAggregator 
 {
-	//private Logger log = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
-		//ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("error-config.xml");
-		//, ErrorDemo.class
-		//ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/error-aggregator-config.xml");
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("nestedAggregator.xml");
-		//, App.class
-		context.registerShutdownHook();
-
 		ErrorDemoGateway gateway = context.getBean(ErrorDemoGateway.class);
 		
 		// This list is too short (length < 3) and the list filter will catch the error.
@@ -38,13 +31,10 @@ public class nestedAggregator
 		// Words must have at least 6 characters, otherwise the word filter will catch the error.
 		List<String> wordList2 = Arrays.asList("Veritas", "omnia", "vincit"); 
 		
-		//gateway.process( Arrays.asList( wordList1 )  );	
 		gateway.process( Arrays.asList( wordList1, wordList2 )  );
-		context.close();
     }
     
 	public interface ErrorDemoGateway {
 		public Object process(List<List<String>> list);
-		//public Future process(List<List<String>> list);
 	}
 }
